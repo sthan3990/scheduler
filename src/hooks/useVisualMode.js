f07a5fdaae56c947555519e5152990edc9d16472
phankons <stephen.han@gmx.com>
2023-09-06 12:28:15 -0400

    Adding Back

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,11 +1,16 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); // This line is new!
 
-  function transition(newMode){
-    setMode(newMode)
+  function transition(newMode) {
+    setHistory(prev => [...prev, newMode]);
   }
 
-  return { mode, transition }
-};
+  function back() {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+  }
+
+  return { mode: history[history.length - 1], transition, back };
+}
+
