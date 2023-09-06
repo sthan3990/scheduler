679b24521b02c74b87da562aad861d0849338f8b
phankons <stephen.han@gmx.com>
2023-09-06 12:38:05 -0400

    Adding Limits

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,12 +3,19 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]); // This line is new!
 
-  function transition(newMode) {
-    setHistory(prev => [...prev, newMode]);
+  function transition(newMode, result = false) {
+
+    if (!result) {
+      setHistory(prev => [...prev, newMode]);
+    }
+
+    setHistory(prev => [...prev.slice(0, -1), newMode]);
   }
 
   function back() {
-    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+    if (history.length > 1) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+    }
   }
 
   return { mode: history[history.length - 1], transition, back };
